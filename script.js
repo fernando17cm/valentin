72f3828353029d11f59e6048c7eaed0b9e9279c8
fernando17cm <fernandotrochez69@gmail.com>
2025-02-05 23:48:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-function handleYesClick() {
-    alert("¡Sabía que dirías que sí! 💖🥰");
-}
 
-function moveButton() {
-    let button = document.querySelector(".no-button");
-    let maxX = window.innerWidth - 150;
-    let maxY = window.innerHeight - 100;
-
-    let x = Math.random() * maxX;
-    let y = Math.random() * maxY;
-
-    button.style.left = `${x}px`;
-    button.style.top = `${y}px`;
-}
